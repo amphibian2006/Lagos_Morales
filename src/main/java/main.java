695738dc58bb2cf98d3ead2344243e418205f4de
Ignacio Morales <70661952+amphibian2006@users.noreply.github.com>
2022-04-13 09:48:37 -0400

    Segundo commit

---
+++ src/main/java/main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class main {
+    static Scanner leer = new Scanner(System.in);
+    public static void main(String[] args) {
+        menuSismos();
+    }
+
+    private static void menuSismos() {
+        int n=0;
+        do{
+        System.out.println("¿Cuantos dias quiere ver? ");
+        n=leer.nextInt();
+        }while (n<2 || n>24);
+        double[][] sensorSismos = new double[n][(int) 24];
+        lecturaSismos(sensorSismos);
+        mostrarSismos(sensorSismos);
+        Menu(sensorSismos);
+    }
+
+    private static void Menu(double[][] sensorSismos) {
+        int eleccion=0;
+        System.out.println("===== MENÚ =====");
+        System.out.println("[1] Mostrar sismo más intenso");
+        System.out.println("[2] Mostrar sismos ≥ 4.0 grados Richter");
+        System.out.println("[3] Reiniciar el arreglo con nuevos valores");
+        System.out.println("[4] Salir");
+        switch (eleccion){
+            case 1:
+                MaxSismo(sensorSismos);
+                break;
+            case 2:
+
+                break;
+            case 3:
+                lecturaSismos(sensorSismos);
+                break;
+            case 4:
+                break;
+        }
+    }
+
+    public static void MaxSismo(double[][] sensorSismos) {
+        double Max=0;
+        for(int i=0;i<sensorSismos.length;i++ ){
+            for (int j = 0; j < sensorSismos[i].length; j++) {
+                 Max=Math.max(sensorSismos[i][j],sensorSismos[(i+1)][j]);
+                 }
+            }
+        System.out.println("El mayor sismo es: "+Max);
+        }
+
+    public static void mostrarSismos(double[][] sensorSismos) {
+        for (int i = 0; i < sensorSismos.length; i++) {
+            for (int j = 0; j < sensorSismos[i].length; j++) {
+                System.out.print("["+sensorSismos[i][j]+"]");
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void lecturaSismos(double[][] sensorSismos) {
+        for (int i = 0; i < sensorSismos.length; i++) {
+            for (int j = 0; j < sensorSismos[0].length; j++) {
+                double numeroAleatorio = (Math.random() * 8.5) + 1;
+                sensorSismos[i][j] = numeroAleatorio;
+            }
+        }
+    }
+}
