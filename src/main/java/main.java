fcc857b6e764eefdcb90cfa3398c86b1f97bf4ab
Ignacio Morales <70661952+amphibian2006@users.noreply.github.com>
2022-04-13 10:31:52 -0400

    Cuarto commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,56 +1,91 @@
+import java.util.NoSuchElementException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
     static Scanner leer = new Scanner(System.in);
     public static void main(String[] args) {
+        try{
         menuSismos();
-    }
-
-    private static void menuSismos() {
-        int n=0;
-        do{
-        System.out.println("¿Cuantos dias quiere ver? ");
-        n=leer.nextInt();
-        }while (n<2 || n>24);
-        double[][] sensorSismos = new double[n][(int) 24];
-        lecturaSismos(sensorSismos);
-        mostrarSismos(sensorSismos);
-        Menu(sensorSismos);
-    }
-
-    private static void Menu(double[][] sensorSismos) {
-        int eleccion=0;
-        System.out.println("===== MENÚ =====");
-        System.out.println("[1] Mostrar sismo más intenso");
-        System.out.println("[2] Mostrar sismos ≥ 4.0 grados Richter");
-        System.out.println("[3] Reiniciar el arreglo con nuevos valores");
-        System.out.println("[4] Salir");
-        eleccion=leer.nextInt();
-        switch (eleccion){
-            case 1:
-                MaxSismo(sensorSismos);
-                break;
-            case 2:
-                MayoresSismos(sensorSismos);
-                break;
-            case 3:
-                lecturaSismos(sensorSismos);
-                break;
-            case 4:
-                break;
+        }catch (Exception e){
+            System.out.println("Valor no valido");
         }
     }
 
-    private static void MayoresSismos(double[][] sensorSismos) {
+    private static void menuSismos() {
+        int n;
+            do {
+                System.out.println("¿Cuantos dias quiere ver? ");
+                n = leer.nextInt();
+                double[][] sensorSismos = new double[n][(int) 24];
+                lecturaSismos(sensorSismos);
+                mostrarSismos(sensorSismos);
+                Menu(sensorSismos);
+            } while (n < 2 || n > 31);
+    }
 
+    private static void Menu(double[][] sensorSismos) {
+        int eleccion;
+        do {
+            System.out.println("===== MENÚ =====");
+            System.out.println("[1] Mostrar sismo más intenso");
+            System.out.println("[2] Mostrar sismos ≥ 4.0 grados Richter");
+            System.out.println("[3] Reiniciar el arreglo con nuevos valores");
+            System.out.println("[4] Salir");
+            eleccion = leer.nextInt();
+            switch (eleccion) {
+                case 1:
+                    MaxSismo(sensorSismos);
+                    break;
+                case 2:
+                    sismosMayores(sensorSismos);
+                    break;
+                case 3:
+                    lecturaSismos(sensorSismos);
+                    mostrarSismos(sensorSismos);
+                    break;
+                case 4:
+                    break;
+            }
+        }while (eleccion!=4);
+    }
+
+    public static void sismosMayores(double[][] sensorSismos) {
+        int[] sismosPorDia = contarSismosMayores(sensorSismos);
+        mostrarSismosMayores(sismosPorDia);
+    }
+
+    public static int[] contarSismosMayores(double[][] sensorSismos) {
+        int[] sismosPorDia = new int[sensorSismos.length];
+        for (int i = 0; i < sensorSismos.length; i++) {
+            int contador = 0;
+            for (int j = 0; j < sensorSismos[0].length; j++) {
+                if(sensorSismos[i][j] >= 4.0){
+                    contador++;
+                }
+            }
+            sismosPorDia[i] = contador;
+        }
+        return sismosPorDia;
+    }
+
+    public static void mostrarSismosMayores(int[] sismosPorDia) {
+        for (int i = 0; i < sismosPorDia.length; i++) {
+            System.out.println("Día "+(i+1)+" = "+sismosPorDia[i]+" veces");
+        }
     }
 
     public static void MaxSismo(double[][] sensorSismos) {
         double Max=0;
+        int dia;
+        int hora;
         for(int i=0;i<sensorSismos.length;i++ ){
             for (int j = 0; j < sensorSismos[i].length; j++) {
                 if(sensorSismos[i][j]>Max){
                     Max=sensorSismos[i][j];
+                    if(Max==0){
+
+                    }
                 }
                  }
             }
